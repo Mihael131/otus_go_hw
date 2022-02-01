508c2ba956ca107cfd95eb1b0337c37c7392177b
mg <mihaelgurzo@gmail.com>
2022-02-01 15:27:33 +0300

    hw_02 - new unit test

--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -33,6 +33,24 @@ func TestUnpack(t *testing.T) {
 	}
 }
 
+func TestUnpackSpecial(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "ТакЪ0 то3ж2е1 можно!3", expected: "Так тооожже можно!!!"},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
 func TestUnpackInvalidString(t *testing.T) {
 	invalidStrings := []string{"3abc", "45", "aaa10b"}
 	for _, tc := range invalidStrings {
