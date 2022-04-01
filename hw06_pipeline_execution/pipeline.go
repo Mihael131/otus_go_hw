ecb91900305c72bbb1580429d9727edb5bcf6c34
mg <mihaelgurzo@gmail.com>
2022-04-01 19:22:16 +0300

    hw_06 - done

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,6 +9,28 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+	if len(stages) > 1 {
+		out = ExecutePipeline(in, done, stages[:len(stages)-1]...)
+	}
+	return worker(done, stages[len(stages)-1](out))
+}
+
+func worker(done In, in In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				return
+			case v, ok := <-in:
+				if !ok {
+					return
+				}
+				out <- v
+			}
+		}
+	}()
+	return out
 }
