9efd759687ba11175bb47e9396d5b859467827be
mg <mihaelgurzo@gmail.com>
2022-04-06 22:08:10 +0300

    hw_08 - done

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"io/ioutil"
+	"strings"
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -11,6 +16,29 @@ type EnvValue struct {
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	env := make(Environment)
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, file := range files {
+		filename := file.Name()
+		if strings.ContainsRune(filename, '=') {
+			// the name S must not contain =
+			continue
+		}
+		str, _ := ioutil.ReadFile(dir + "/" + filename)
+		env[filename] = getEnvValue(str)
+	}
+	return env, nil
+}
+
+func getEnvValue(b []byte) EnvValue {
+	split := strings.Split(string(b), "\n")
+	str := strings.TrimRight(split[0], "\t ")
+	str = strings.ReplaceAll(str, "\x00", "\n")
+	return EnvValue{
+		str,
+		len(str) == 0,
+	}
 }
--- hw08_envdir_tool/executor_test.go
+++ hw08_envdir_tool/executor_test.go
@@ -1,7 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestRunCmd(t *testing.T) {
-	// Place your code here
+	t.Run("command success", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+			"testdata/echo.sh",
+		}
+
+		code := RunCmd(cmd, nil)
+
+		require.Equal(t, code, 0)
+	})
+
+	t.Run("command not found", func(t *testing.T) {
+		cmd := []string{
+			"/bin/bash",
+			"testdata/echo2.sh",
+		}
+
+		code := RunCmd(cmd, nil)
+
+		require.Equal(t, code, 127)
+	})
 }
